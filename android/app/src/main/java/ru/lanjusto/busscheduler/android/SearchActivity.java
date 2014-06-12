62333abf165490f86bc95e7dc96f04de0980b4dd
estroykov <lanjusto@ya.ru>
2014-06-12 20:48:21 +0400

    Issue 13: Прототип экрана поиска

---
+++ android/app/src/main/java/ru/lanjusto/busscheduler/android/SearchActivity.java
@@ -0,0 +1,15 @@
+package ru.lanjusto.busscheduler.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+}
